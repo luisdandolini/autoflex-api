50da42d658f93d04bc19c564320bfac38b128671
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-02-16 22:13:43 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import { errorHandler } from "./app/middlewares/errorHandler";
 const app = express();
 const port = 3000;
 
+const cors = require("cors");
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use(routes);
