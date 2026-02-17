6465aa322f3b099b49c9d69c7415d544261420e5
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-02-17 01:31:54 -0300

    fix: db connection for both environments

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,16 +4,27 @@ const { Pool } = pkg;
 
 dotenv.config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "postgres",
-  database: process.env.DB_NAME || "autoflex",
-});
+const poolConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: process.env.DB_HOST || "localhost",
+      port: Number(process.env.DB_PORT) || 5432,
+      user: process.env.DB_USER || "postgres",
+      password: process.env.DB_PASSWORD || "postgres",
+      database: process.env.DB_NAME || "autoflex",
+    };
+
+const pool = new Pool(poolConfig);
 
 pool.on("connect", () => {
-  console.log("Database connected");
+  console.log(
+    `Database connected (${process.env.DATABASE_URL ? "Production" : "Local"})`,
+  );
 });
 
 pool.on("error", (err) => {
